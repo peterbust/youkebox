d88e4dc44985b5115fa4d1e6ebf0343603ba5886
peterbust <peterbust@me.com>
2019-06-06 08:43:59 +0200

    Renamed library to songs

---
+++ src/components/YbSongsList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="$style.list">
+    <YbSongListItem
+      v-for="song in songs"
+      :key="song.title"
+      :title="song.title"
+    />
+  </div>
+</template>
+
+<script>
+import YbSongListItem from './YbSongListItem.vue'
+
+export default {
+  components: { YbSongListItem },
+
+  computed: {
+    songs() {
+      return this.$store.state.songs
+    },
+  },
+}
+</script>
+
+<style module>
+  .list {
+    width: 100%;
+    height: 50%;
+    overflow: hidden;
+  }
+</style>
--- src/store.js
+++ src/store.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import library from '@/state/library.json'
+import songs from '@/state/songs.json'
 import queue from '@/state/queue.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    library,
+    songs,
     queue,
   },
 
   getters: {
     getSongByReference: state => songReference => ({
-      ...state.library.find(track => track.title === songReference.title),
+      ...state.songs.find(song => song.title === songReference.title),
       ...songReference,
     }),
   },
